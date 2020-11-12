35d4f82bc10ff45dc86f8d74805940752ab0b274
BLIA THAO <blia451054@mail.fresnostate.edu>
2020-11-12 15:24:03 -0800

    finished Pseudo-code

--- IS51Test1.py
+++ IS51Test1.py
@@ -12,3 +12,30 @@ If the amount is equal, we output to the user, "Option 1 and Option 2 pays the s
 If option1 is better, we output to the user "Option 1 is better"
 if Option2 is bette, we output to the user "Option 2 is better"
 """
+
+
+"""
+# Option1
+return 100 * 10
+
+# Option2
+amount = 1
+list1 = []
+loop 10 times 
+    add amount to list1
+    amount *= 2
+sum = sum all items in the loop
+return sum
+
+# main
+    var1 = option1
+    var2 = option2
+
+if var1= var2
+    "Option 1 and Option 2 pays the same"
+if var1 < var2, we output to the user "Option 2 is better"
+else 
+    "Option 1 is better"
+
+Main
+"""
